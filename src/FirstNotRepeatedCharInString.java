30e6ad5b562e2dbf13ea6275ff32aa442b45206f
akrzos <arkadiusz666@gmail.com>
2016-06-09 13:27:33 +0200

    initaial

--- src/FirstNotRepeatedCharInString.java
+++ src/FirstNotRepeatedCharInString.java
@@ -1,12 +1,31 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Created by akrzos on 2016-06-07.
  */
 public class FirstNotRepeatedCharInString {
 
-    char findFirstNotRepeatedCharInString(String string) {
-        char wynik = ' ';
+    static char findFirstNotRepeatedCharInString(String string) {
+        Map<Character, Integer> mapa= new LinkedHashMap<>();
+        for (int i = 0; i < string.length(); i++) {
+            if (mapa.containsKey(string.charAt(i))) {
+                mapa.put(string.charAt(i),2);
+            } else {
+                mapa.put(string.charAt(i),1);
+            }
+        }
+        for (Character character : mapa.keySet()) {
+            if (mapa.get(character)==1) {
+                return  character;
+            }
+        }
+        return ' ';
+    }
 
-        return wynik;
+    public static void main(String[] args) {
+        String test= "121JestemB2urakiem";
+        System.out.println("First non repeated "  +findFirstNotRepeatedCharInString(test));
     }
 
 }
---
+++ src/LogicOperatorTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created by akrzos on 2016-06-08.
+ */
+public class LogicOperatorTest {
+
+
+    public static void main(String[] args) {
+        boolean prawda = true;
+        boolean falsz = false;
+        System.out.println(prawda & falsz);
+        System.out.println(prawda && falsz);
+        System.out.println(prawda && falsz);
+
+    }
+}
